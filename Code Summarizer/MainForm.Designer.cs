6bb516d6d45137472db651c0365412f24b4bf75b
jbk77 <josephbk.117@hotmail.com>
2017-03-10 20:06:36 +0530

    UI Fixes

--- Code Summarizer/MainForm.Designer.cs
+++ Code Summarizer/MainForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.openFolderPathTextBox = new System.Windows.Forms.TextBox();
             this.outputFolderPathTextBox = new System.Windows.Forms.TextBox();
             this.openFolderButton = new System.Windows.Forms.Button();
@@ -39,7 +40,7 @@
             // openFolderPathTextBox
             // 
             this.openFolderPathTextBox.Font = new System.Drawing.Font("Lucida Sans Unicode", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.openFolderPathTextBox.Location = new System.Drawing.Point(12, 34);
+            this.openFolderPathTextBox.Location = new System.Drawing.Point(11, 20);
             this.openFolderPathTextBox.Name = "openFolderPathTextBox";
             this.openFolderPathTextBox.Size = new System.Drawing.Size(461, 27);
             this.openFolderPathTextBox.TabIndex = 0;
@@ -47,57 +48,63 @@
             // outputFolderPathTextBox
             // 
             this.outputFolderPathTextBox.Font = new System.Drawing.Font("Lucida Sans Unicode", 9.75F);
-            this.outputFolderPathTextBox.Location = new System.Drawing.Point(12, 94);
+            this.outputFolderPathTextBox.Location = new System.Drawing.Point(11, 85);
             this.outputFolderPathTextBox.Name = "outputFolderPathTextBox";
             this.outputFolderPathTextBox.Size = new System.Drawing.Size(461, 27);
             this.outputFolderPathTextBox.TabIndex = 1;
             // 
             // openFolderButton
             // 
-            this.openFolderButton.Font = new System.Drawing.Font("Roboto Condensed", 9.75F, System.Drawing.FontStyle.Bold);
-            this.openFolderButton.Location = new System.Drawing.Point(482, 34);
+            this.openFolderButton.BackColor = System.Drawing.Color.White;
+            this.openFolderButton.Font = new System.Drawing.Font("Orbitron", 11.25F, System.Drawing.FontStyle.Bold);
+            this.openFolderButton.Location = new System.Drawing.Point(481, 20);
             this.openFolderButton.Name = "openFolderButton";
-            this.openFolderButton.Size = new System.Drawing.Size(187, 23);
+            this.openFolderButton.Size = new System.Drawing.Size(187, 27);
             this.openFolderButton.TabIndex = 2;
             this.openFolderButton.Text = "Open Folder";
-            this.openFolderButton.UseVisualStyleBackColor = true;
+            this.openFolderButton.UseVisualStyleBackColor = false;
             this.openFolderButton.Click += new System.EventHandler(this.OpenFolderButton_Click);
             // 
             // outputFolderButton
             // 
-            this.outputFolderButton.Font = new System.Drawing.Font("Roboto Condensed", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.outputFolderButton.Location = new System.Drawing.Point(482, 94);
+            this.outputFolderButton.BackColor = System.Drawing.Color.White;
+            this.outputFolderButton.Font = new System.Drawing.Font("Orbitron", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputFolderButton.Location = new System.Drawing.Point(481, 85);
             this.outputFolderButton.Name = "outputFolderButton";
-            this.outputFolderButton.Size = new System.Drawing.Size(187, 23);
+            this.outputFolderButton.Size = new System.Drawing.Size(187, 27);
             this.outputFolderButton.TabIndex = 3;
             this.outputFolderButton.Text = "Output Folder";
-            this.outputFolderButton.UseVisualStyleBackColor = true;
+            this.outputFolderButton.UseVisualStyleBackColor = false;
             this.outputFolderButton.Click += new System.EventHandler(this.OutputFolderButton_Click);
             // 
             // comboBox
             // 
+            this.comboBox.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox.FormattingEnabled = true;
-            this.comboBox.Location = new System.Drawing.Point(14, 67);
+            this.comboBox.Location = new System.Drawing.Point(11, 53);
             this.comboBox.Name = "comboBox";
-            this.comboBox.Size = new System.Drawing.Size(655, 21);
+            this.comboBox.Size = new System.Drawing.Size(657, 26);
             this.comboBox.TabIndex = 4;
             // 
             // summaryGenerateButton
             // 
-            this.summaryGenerateButton.Font = new System.Drawing.Font("Roboto Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.summaryGenerateButton.Location = new System.Drawing.Point(232, 127);
+            this.summaryGenerateButton.BackColor = System.Drawing.Color.White;
+            this.summaryGenerateButton.Font = new System.Drawing.Font("Orbitron", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.summaryGenerateButton.Location = new System.Drawing.Point(160, 127);
             this.summaryGenerateButton.Name = "summaryGenerateButton";
-            this.summaryGenerateButton.Size = new System.Drawing.Size(205, 44);
+            this.summaryGenerateButton.Size = new System.Drawing.Size(312, 44);
             this.summaryGenerateButton.TabIndex = 5;
             this.summaryGenerateButton.Text = "Generate Code Summary";
-            this.summaryGenerateButton.UseVisualStyleBackColor = true;
+            this.summaryGenerateButton.UseCompatibleTextRendering = true;
+            this.summaryGenerateButton.UseVisualStyleBackColor = false;
             this.summaryGenerateButton.Click += new System.EventHandler(this.SummaryGenerateButton_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(680, 183);
             this.Controls.Add(this.summaryGenerateButton);
             this.Controls.Add(this.comboBox);
@@ -107,11 +114,12 @@
             this.Controls.Add(this.openFolderPathTextBox);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.MinimumSize = new System.Drawing.Size(50, 50);
             this.Name = "MainForm";
-            this.Text = "Main Form";
+            this.Text = "Code Summarizer";
             this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.ResumeLayout(false);
             this.PerformLayout();
--- Code Summarizer/Properties/Resources.Designer.cs
+++ Code Summarizer/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace Code_Summarizer.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap GUIScriptSummary10 {
-            get {
-                object obj = ResourceManager.GetObject("GUIScriptSummary10", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
